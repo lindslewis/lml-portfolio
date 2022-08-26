62e7c57798c8be50a38ca63487f63df8b55f3b6b
LindsayLew <lmschwehr@gmail.com>
2022-08-25 21:30:11 -0700

    base navbar, many changes to make

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,11 +7,34 @@ import React from 'react';
 
 function Navbar({ currentPage, handleChange }) {
     return (
-        <ul>
-            <li>
+        <ul className='nav nav-tabs'>
+            <li className='nav-item'>
                 <a href='#home'
                 onClick={() => handleChange('Home')}
-                className={ currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+                className = { currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>
+                    Home
+                </a>
+            </li>
+            <li className='nav-item'>
+                <a href='#about'
+                onClick={() => handleChange('About')} className = { currentPage === 'About' ? 'nav-link active' : 'nav-link'}>
+                    About
+                </a>
+            </li>
+            <li className='nav-item'>
+                <a href='#portfolio'
+                onClick={() => handleChange('Portfolio')}
+                className = { currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>
+                    {/* would like to change the names of all these but they'll work for now */}
+                    Portfolio
+                </a>
+            </li>
+            <li className='nav-item'>
+                <a href='#contact'
+                onClick={() => handleChange('Contact')}
+                className = { currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>
+                    Contact
+                </a>
             </li>
         </ul>
     )
