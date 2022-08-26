2fbf6cd6cc2d7a1dc6263eda7757bb1f323cf4e3
LindsayLew <lmschwehr@gmail.com>
2022-08-25 20:58:04 -0700

    set up base for about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,3 +1,12 @@
 // import react
 
 // need to export the about page? probably not a whole lot here.
+
+import React from 'react';
+
+export default function About() {
+    return (
+        <h1>About Placeholder</h1>
+        // this is where my html will go
+    );
+}
