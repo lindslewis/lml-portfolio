591314343dbdf61cc1f474e2496799688c33c22f
LindsayLew <lmschwehr@gmail.com>
2022-08-25 20:56:34 -0700

    app

--- src/App.js
+++ src/App.js
@@ -2,3 +2,10 @@
 // import port container 
 // call/set app
 // export app
+
+import React from 'react';
+import portContainer from './components/portContainer';
+
+const App = () => <portContainer />;
+
+export default App;
