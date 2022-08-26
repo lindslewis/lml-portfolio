9f41cf43be193182883789a158975e3d5db5e756
LindsayLew <lmschwehr@gmail.com>
2022-08-25 20:55:12 -0700

    index initial

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@
 // import App
 // import css framework
 // react-dom render app
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+// css frame work??
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// not sure what else I need at this point, so moving on
