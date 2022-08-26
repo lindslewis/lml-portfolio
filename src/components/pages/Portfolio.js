e0a25b6dee2395479a23f4591b0d8d2b6774393d
LindsayLew <lmschwehr@gmail.com>
2022-08-25 21:00:29 -0700

    base code for port and home:

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,3 +2,11 @@
 
 // all the stuffs, so gotta layout my page look
 // export
+
+import React from 'react';
+
+export default function Portfolio() {
+    return (
+        <h1>Port Placeholder</h1>
+    );
+}
