3e5b2740a2c386b48608cb60e29830050ba279cf
LindsayLew <lmschwehr@gmail.com>
2022-08-25 21:23:19 -0700

    added boot

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// css frame work??
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
