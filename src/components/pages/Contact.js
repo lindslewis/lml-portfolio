35733e3c594a6c96a887abc790f03d071ed943dd
LindsayLew <lmschwehr@gmail.com>
2022-09-03 18:08:08 -0700

    contact styling start

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,17 +5,33 @@
 
 import React from 'react';
 import { Form } from 'react-bootstrap';
+import '../style/contact.css'
 
 export default function Contact() {
     return (
         <section className='pageCon'>
+            <h1 id='title'>Contact Me</h1>
             <Form>
+                <Form.Group controlId='formName'>
+                    <Form.Label>
+                        Your Name
+                    </Form.Label>
+                    <Form.Control placeholder='Enter Name Here'>
+                    </Form.Control>
+                </Form.Group>
                 <Form.Group controlId='formEmail'>
                     <Form.Label>
                         Email Address
                     </Form.Label>
                     <Form.Control type='email' placeholder='Enter Email Here'/>
                 </Form.Group>
+                <Form.Group controlId='formText'>
+                    <Form.Label>
+                        Enter your message here.
+                    </Form.Label>
+                    <Form.Control as="textarea" rows={5} />
+                </Form.Group>
+
             </Form>
         </section>
 
