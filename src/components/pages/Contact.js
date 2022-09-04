097115c4f32696796e3f1c6324eaca55fece030e
LindsayLew <lmschwehr@gmail.com>
2022-09-03 21:55:24 -0700

    proficiencies list and resume download

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -81,6 +81,37 @@ export default function Contact() {
                 </Form.Group>
 
             </Form>
+            <section id="resumeCon">
+                <section id='downloadCon'>
+                    <p>
+                        Download my resume!
+                    </p>
+                    <Button id="resumeBtnCon">
+                        <a id="resumeBtn" href="https://acrobat.adobe.com/id/urn:aaid:sc:VA6C2:09d83dc1-97c0-4a02-9824-b73410d05464">Download</a>
+                    </Button>
+                </section>
+                <h3>Proficiencies</h3>
+                <ul>
+                    <li>
+                        HTML
+                    </li>
+                    <li>
+                        CSS
+                    </li>
+                    <li>
+                        Javascript
+                    </li>
+                    <li>
+                        React
+                    </li>
+                    <li>
+                        Writing
+                    </li>
+                    <li>
+                        Copy Editing
+                    </li>
+                </ul>
+            </section>
         </section>
 
     );
