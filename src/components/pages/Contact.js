ff0ba97f647e9c6bd41cc454b1737f29e0b27f72
LindsayLew <lmschwehr@gmail.com>
2022-09-03 17:47:25 -0700

    email form group"

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,9 +4,20 @@
 // export
 
 import React from 'react';
+import { Form } from 'react-bootstrap';
 
 export default function Contact() {
     return (
-        <h1>Contact Placeholder</h1>
+        <section className='pageCon'>
+            <Form>
+                <Form.Group controlId='formEmail'>
+                    <Form.Label>
+                        Email Address
+                    </Form.Label>
+                    <Form.Control type='email' placeholder='Enter Email Here'/>
+                </Form.Group>
+            </Form>
+        </section>
+
     );
 }
