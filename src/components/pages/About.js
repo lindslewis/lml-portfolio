66d626b291472a35375429edae1b8da456e5be43
LindsayLew <lmschwehr@gmail.com>
2022-09-03 22:13:52 -0700

    avatar is showing, need to resize

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,12 +1,24 @@
 // import react
 
 // need to export the about page? probably not a whole lot here.
-
+import '../style/about.css'
 import React from 'react';
+import { Image } from 'react-bootstrap';
 
 export default function About() {
     return (
-        <h1>About Placeholder</h1>
+        <section id='pageCon'>
+            <section id='introCon'>
+                <h1>Hi there, I'm Lindsay</h1>
+                <h3>I'm a full-stack developer, excited to create some beautiful apps.</h3>
+            </section>
+            <section id='avatarCon'>
+                <Image src={require("../avatar.jpg")} alt='avatar of Lindsay'></Image>
+            </section>
+            
+            
+        </section>
+        
         // this is where my html will go
     );
 }
