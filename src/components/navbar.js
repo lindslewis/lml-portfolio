b8eb5c228b5535ccdd0ae9149f435358706d74b8
LindsayLew <lmschwehr@gmail.com>
2022-09-04 20:13:03 -0700

    it works :) background for footer is fighting me though :(

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,33 +2,14 @@
 
 // more imports??
 // this will structure the usage of the navbar and the tabs, likely what's active or not, and a decent chunk of page changes I assume??
-import Image from 'react-bootstrap/Image'
+
 import React from 'react';
 import './style/header.css'
 
 function Navbar({ currentPage, handleChange }) {
     return (
     <section className="header p-2" role="navigation">
-        <section className='headerCon'>
-            <section className='figure'>
-                <a href="https://www.linkedin.com/in/lewis-lindsay/">
-                    <Image src="https://cdn.jsdelivr.net/npm/simple-icons@7.8.0/icons/linkedin.svg" alt="LinkedIn badge"></Image>
-                </a>
-            </section>
 
-            <section className='figure'>
-                <a href="https://stackoverflow.com/users/19833916/lindslewis">
-                    <Image src="https://cdn.jsdelivr.net/npm/simple-icons@7.8.0/icons/stackoverflow.svg" alt="Stack Overflow badge"></Image>
-                </a>
-            </section>
-
-            <section className='figure'>
-                <a href="https://github.com/lindslewis">
-                    <Image src="https://cdn.jsdelivr.net/npm/simple-icons@7.8.0/icons/github.svg" alt="Github badge"></Image>
-                </a>
-            </section>
-       
-        </section>
         <ul className='nav nav-tabs mt-1'>
             {/* <li className='nav-item'>
                 <a href='#home'
@@ -67,7 +48,9 @@ function Navbar({ currentPage, handleChange }) {
             </li>
 
         </ul>
+
     </section>
+    
         
     );
 }
--- src/components/portContainer.js
+++ src/components/portContainer.js
@@ -13,7 +13,10 @@ import Contact from './pages/Contact';
 import Portfolio from './pages/Portfolio';
 import Navbar from './Navbar';
 import Resume from './pages/Resume';
+import Image from 'react-bootstrap/Image'
 
+
+import './style/header.css'
 // starting on home :)
 export default function PortContainer() {
     const [ currentPage, setCurrent ] = useState('Home');
@@ -39,10 +42,35 @@ export default function PortContainer() {
                 {/* <Routes>
                     <Route> */}
                     <Navbar currentPage = {currentPage} handleChange = {handleChange} />
+                 
                     {/* </Route>
                 </Routes> */}
 
             {renderPage()}
+            <footer id="footer">
+                        <ul id="footerUl">
+                            <li className='footerList'>
+                                <a href="https://stackoverflow.com/users/19833916/lindslewis">
+                                    <Image className="figure" src="https://cdn.jsdelivr.net/npm/simple-icons@7.8.0/icons/stackoverflow.svg" alt="Stack Overflow badge"></Image>
+                                    {/* <FontAwesomeIcon className="figure" icon="fa-brands fa-stack-overflow" /> */}
+                                </a>
+                            </li>
+                            <li className='footerList'>
+                                <a href="https://github.com/lindslewis">
+                                    <Image className="figure" src="https://cdn.jsdelivr.net/npm/simple-icons@7.8.0/icons/github.svg" alt="Github badge"></Image>
+                                   
+                                </a>
+                            </li>
+                            <li className='footerList'>
+                                <a href="https://www.linkedin.com/in/lewis-lindsay/">
+                                    <Image className="figure" src="https://cdn.jsdelivr.net/npm/simple-icons@7.8.0/icons/linkedin.svg" alt="LinkedIn badge"></Image>
+                                    {/* <FontAwesomeIcon className="figure" icon="fa-brands fa-linkedin" /> */}
+                                </a>
+                            </li>
+
+                        </ul>
+
+                    </footer>
         </section>
     );
 }
