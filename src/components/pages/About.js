7072fb937ea6664a500984e318673ab634b0d4fd
LindsayLew <lmschwehr@gmail.com>
2022-10-19 16:56:00 -0700

    styling about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,24 +3,31 @@ import '../style/about.css'
 
 export default function About() {
     return (
-        <section>
-            <h1>
-                Hi there!
-            </h1>
-            <h1>I'm Lindsay.</h1>
-            <p>
-                I'm so glad that you are here! 
-            </p>
-            <p>
+        <section id='aboutCon'>
+            <section id="accentIntro">
+                <p id="intro">
+                    Hi there!
+                </p>
+                <p id="myName">
+                    I'm
+                </p>
+                <p id="introName">
+                    Lindsay
+                </p>
+                <p id="gladAb">
+                    I'm so glad that you are here! 
+                </p>
+            </section>
+            <p id="devAb">
                 I'm a full-stack developer and recent University of Washington Coding Bootcamp graduate. I'm always looking for a challenge and opportunities to grow, not only as a developer, but as a human. 
             </p>
-            <p> 
+            <p id="backgroundAb"> 
                 Having a background in English, I offer a lot of flexibility and differing perspectives to projects.   
             </p>
-            <p>
+            <p id="aboutMe">
                 I love working in the front-end, playing with colors, and pushing the envelope on my own skills. I prefer to go above and beyond the expectations set for me, and I have the perseverence to make that happen.
             </p>
-            <p>
+            <p id="aboutEnd">
                 Want to talk? Feel free to reach out!
             </p>
         </section>
